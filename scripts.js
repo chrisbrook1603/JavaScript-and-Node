334d47b7d1ec7123050d18713e0f80f04018cf20
chrisbrook1603 <christopher.brook@outlook.com>
2021-09-29 11:16:52 +0100

    new variable versioN

--- scripts.js
+++ scripts.js
@@ -1,17 +1,9 @@
-// this is a simple console logging statement
-console.log("Hello world!");
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
 
-// different kinds of console logging
+// Use interpolation for operations in string
+console.log(`The total bill is £4000 the remaining amount of money to be paid is £${totalMoney - moneyPaidSoFar}`);
 
-let persondetails = {firstname : "Chris",
-    surname : "Brook",
-    place : "Huddersfield",
-    starsign : "Aquarius" };
-
-console.log(persondetails.firstname);
-console.log(persondetails.surname);
-console.log(persondetails.place);
-console.log(persondetails.starsign);
-
-// html styling for console
-console.log(`%c` + "Hi, my name is " + persondetails.firstname + " " + persondetails.surname + ". I am from " + persondetails.place + " and my star sign is " + persondetails.starsign, 'color: orange; background-color: white; font-size: 10px; font-family: fantasy; padding: 20px;');
+//Use variable and referance using interpolation
+console.log(`The total bill is £4000 the remaining amount of money to be paid is £${totalLeftToPay}`);
